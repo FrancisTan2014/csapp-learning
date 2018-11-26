31e23273cdca2b76fe9977e7bdd4a4014c67c6af
谭光洪 <499166435@qq.com>
2018-11-26 17:45:21 +0800

    ﻿ type: feat
    what: finished exercise 2.27
    
    why:
    
    how:
    
    remarks:

---
+++ ch02/ex.2.27.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int uadd_ok(unsigned x, unsigned y);
+
+int main() {
+    unsigned x = 0xffffffff;
+    unsigned y = 1;
+    int addOk = uadd_ok(x, y);
+    printf("with arguments %u and %u, the result of uadd_ok is: %d\n", x, y, addOk);
+
+    x = 0xffff0000;
+    y = 0x0000ffff;
+    addOk = uadd_ok(x, y);
+    printf("with arguments %u and %u, the result of uadd_ok is: %d", x, y, addOk);
+    return 0;
+}
+
+int uadd_ok(unsigned x, unsigned y) {
+    return x + y >= x;
+}
