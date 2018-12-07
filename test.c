83756a86c52dba18c5854f2dba755146e9064384
谭光洪 <499166435@qq.com>
2018-12-07 09:38:29 +0800

    modified test.c

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-#include <stdint.h>
-#include "ch02/show_byte.h"
+// #include <stdint.h>
+// #include "ch02/show_byte.h"
+
+void assembly_test(int x, int y);
 
 int main() {
-    short x = 12345;
-    short mx = -x;
-    show_bytes((byte_pointer) &x, sizeof(short));
-    show_bytes((byte_pointer) &mx, sizeof(short));
-
+    // short x = 12345;
+    // short mx = -x;
+    // show_bytes((byte_pointer) &x, sizeof(short));
+    // show_bytes((byte_pointer) &mx, sizeof(short));
+    assembly_test(3, 4);
     return 0;
+}
+
+void assembly_test(int x, int y) {
+    int t = x ^ y;
+    printf("y=%d\n", y);
+    printf("x=%d\n", x);
 }
